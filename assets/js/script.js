0a8551ec70b73883af451d4bf83077ec10fc063b
Candela <candy_nazar@hotmail.com>
2020-08-19 14:55:07 -0400

    fifth commit

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+$('.carousel').carousel({
+    interval: 3200,
+    ride: true
+})
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+
+    $('[data-toggle="popover"]').popover();  
+
+});
